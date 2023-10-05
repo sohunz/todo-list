dd9ac7d509dd3a8d3904ec108b669f34b5e7bed6
sohunz <sohunily@gmail.com>
2023-10-05 14:41:39 +0700

    v 1.0.0

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,41 @@
       <h3>TODO LIST</h3>
       <div class="input mt-4 mb-5">
         <input type="text" class="form-control" v-model="newTodo" />
-        <button type="button" class="btn btn-success" @click="addTodo" id="btn-add">Add</button>
+        <button
+          type="button"
+          class="btn btn-success"
+          @click="addTodo"
+          id="btn-add"
+        >
+          Add
+        </button>
       </div>
       <div class="lists">
-        <button @click="deleteAll"  class="btn btn-danger mb-2 delete-all" v-if="this.todos.length > 0">Delete All</button>
-        <div class=" list border border-1 rounded-2 p-2 my-2"  v-for="(todo, index) in todos" :key="index">
-          <div class="title-add w-75">{{ todo.text }}</div>
+        <button
+          @click="deleteAll"
+          class="btn btn-danger mb-2 delete-all"
+          v-if="this.todos.length > 0"
+        >
+          Delete All
+        </button>
+        <div
+          class="list border border-1 rounded-2 p-2 my-2"
+          v-for="(todo, index) in todos"
+          :key="index"
+        >
+          <div class="title-add w-75" :class="{ 'done-task': todo.done }">
+            {{ todo.text }}
+          </div>
           <div class="action" style="display: flex; align-items: center">
-            <button class="done btn btn-primary mx-2">Done</button>
-            <button class="delete btn btn-danger" @click="removeTodo(index)">Delete</button>
+            <button
+              class="done btn btn-primary mx-2"
+              @click="toggleDone(index)"
+            >
+              {{ todo.done ? "Undone" : "Done" }}
+            </button>
+            <button class="delete btn btn-danger" @click="removeTodo(index)">
+              Delete
+            </button>
           </div>
         </div>
       </div>
@@ -25,15 +51,15 @@ export default {
   data() {
     return {
       show: true,
-      newTodo: '',
+      newTodo: "",
       todos: [],
     };
   },
   methods: {
     addTodo() {
-      if (this.newTodo.trim() !== '') {
-        this.todos.push({text: this.newTodo});
-        this.newTodo = '';
+      if (this.newTodo.trim() !== "") {
+        this.todos.push({ text: this.newTodo });
+        this.newTodo = "";
         this.saveToLocalStorage();
       }
     },
@@ -41,16 +67,22 @@ export default {
       this.todos.splice(index, 1);
       this.saveToLocalStorage();
     },
+
+    toggleDone(index) {
+      this.todos[index].done = !this.todos[index].done;
+      this.saveToLocalStorage();
+    },
+
     deleteAll() {
       this.todos.splice(this.todos);
       this.saveToLocalStorage();
     },
     saveToLocalStorage() {
-      localStorage.setItem('todos', JSON.stringify(this.todos));
+      localStorage.setItem("todos", JSON.stringify(this.todos));
     },
   },
   mounted() {
-    const savedTodos = localStorage.getItem('todos');
+    const savedTodos = localStorage.getItem("todos");
     if (savedTodos) {
       this.todos = JSON.parse(savedTodos);
     }
@@ -59,55 +91,67 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=AR+One+Sans&display=swap');
+* {
+    font-family: 'AR One Sans', sans-serif;
+    font-weight: 600;
+}
+.done-task {
+  text-decoration: line-through;
+  color: #777;
+}
 
 #btn-add {
   width: 100px;
   margin-left: 10px;
 }
 .list {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    }
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
 .input {
   display: flex;
   flex-direction: row;
 }
 .wrapper-todo {
   width: 60%;
-  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
+    rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
 }
 
- @media screen and (max-width: 1200px) {
-    .wrapper-todo {
-      width: 70%;
-    }
+@media screen and (max-width: 1200px) {
+  .wrapper-todo {
+    width: 70%;
   }
- @media screen and (max-width: 1000px) {
+}
+@media screen and (max-width: 1000px) {
   .wrapper-todo {
     padding: 0px;
   }
-  .done, .delete, .delete-all {
+  .done,
+  .delete,
+  .delete-all {
     font-size: 13px;
     margin-top: 10px;
   }
-    .wrapper-todo {
-      width: 90%;
-    }
-    .title-add {
-      font-size: 14px;
-      padding: 0 10px;
-      display: flex;
-      align-items: center;
-    }
-
-    .list .action {
-      padding: 10px 0;
-      display: flex;
-      justify-content: space-between;
-    }
+  .wrapper-todo {
+    width: 90%;
   }
- @media screen and (max-width: 500px) {
+  .title-add {
+    font-size: 14px;
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+  }
+
+  .list .action {
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+  }
+}
+@media screen and (max-width: 500px) {
   .wrapper-todo {
     padding: 0px;
   }
@@ -118,35 +162,35 @@ export default {
     font-size: 10px;
     margin-top: 10px;
   }
-    .wrapper-todo {
-      width: 90%;
-    }
-    .input {
-      display: flex;
-      flex-direction: column;
-    }
-    .title-add {
-      font-size: 12px;
-      padding: 0 10px;
-      display: flex;
-      align-items: center;
-    }
-    .list {
-      display: flex;
-      flex-direction: column;
-    }
-
-    .list .action {
-      padding: 10px 0;
-      display: flex;
-      justify-content: space-between;
-    }
-    #btn-add {
-      width: 100%;
-      height: 35px;
-      margin-left: 0px;
-      font-size: 11px;
-      margin-bottom: -10px;
-    }
+  .wrapper-todo {
+    width: 90%;
   }
+  .input {
+    display: flex;
+    flex-direction: column;
+  }
+  .title-add {
+    font-size: 12px;
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+  }
+  .list {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .list .action {
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-between;
+  }
+  #btn-add {
+    width: 100%;
+    height: 35px;
+    margin-left: 0px;
+    font-size: 11px;
+    margin-bottom: -10px;
+  }
+}
 </style>
